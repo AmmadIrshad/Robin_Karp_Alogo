cbdfe42c42d7cd0da33f3279ce6242da14d4f5d2
Andi Stevenli <ndstevenli@outlook.com>
2025-01-18 11:27:25 +0700

    on progress : initialize Convert Result class to store the result of text recognition

---
+++ lib/model/convert_result.dart
@@ -0,0 +1,7 @@
+class ConvertResult {
+  ///this variable is used to store the result of text recognition
+  final String text;
+
+  ///this is the constructor of [ConvertResult] class
+  ConvertResult({required this.text});
+}
