87860bce46ce67b30acd812dd907aab8c222cba8
hsk-rclone <hashirsaudkhan@gmail.com>
2025-01-30 16:56:40 +0500

    change settings

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter_robin_karp_algorithm_app/controller/home_controller.dart
 import 'package:flutter_robin_karp_algorithm_app/controller/splash_controller.dart';
 import 'package:flutter_robin_karp_algorithm_app/resources/color_manager.dart';
 import 'package:flutter_robin_karp_algorithm_app/resources/unit_manager.dart';
-import 'package:flutter_robin_karp_algorithm_app/view/home/home_screen.dart';
 import 'package:flutter_robin_karp_algorithm_app/view/splash/splash_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
--- lib/resources/text_manager.dart
+++ lib/resources/text_manager.dart
@@ -1,36 +1,3 @@
-// class TextManager {
-//   //for home screen
-//   static const String HOME_TITLE = 'Algoritma Robin Karp';
-//   static const String HOME_CAPTION =
-//       'Ambil gambar yang memiliki objek teks untuk melakukan conversi image to teks.';
-//   static const String HOME_TAKE_PICTURE_BUTTON = 'Ambil gambar';
-//   static const String HOME_COMPARE_BUTTON = 'Compare';
-//   static const String HOME_EXPLANATION =
-//       'Compare teks hasil konversi dengan teks penguji untuk melihat akurasi hasil konversi';
-//   static const String CONVERT_TEXT_BOX_TITLE = 'Hasil Konversi';
-//   static const String TESTER_TEXT_BOX_TITLE = 'Teks Penguji';
-//   static const String CONVERT_TEXT_BOX_HINT =
-//       'Ambil gambar untuk melakukan konversi dalam bentuk teks';
-//   static const String TESTER_TEXT_BOX_HINT = 'Masukan teks untuk pengujian';
-//   //for about screen
-//   static const String ABOUT_TITLE = 'Text me up';
-//   static const String VERSION = 'V.1.0';
-//   static const String DESCRIPTION =
-//       'Aplikasi untuk melakukan konversi image ke teks juga untuk membandingkan akurasi dua buah teks menggunakan robin karp';
-//   static const String MADE_BY_LABEL = 'Dibuat oleh:';
-//   static const String MADE_BY_NAME = 'dea shelpia utami g.s';
-//   //for bottom navigation bar
-//   static const String HOME_NAV_BAR_LABEL = 'Beranda';
-//   static const String ABOUT_NAV_BAR_LABEL = 'About';
-//   //for preview screen
-//   static const String CROP_BUTTON = 'Crop';
-//   static const String CONVERT_BUTTON = 'Convert';
-//   static const String PREVIEW_TITLE = 'Image Preview';
-//   //for internet connection lost
-//   static const String LOST_CONNECTION = 'Oops, Tidak Ada Koneksi Internet';
-//   //for cropper screen
-//   static const String CROPPER_TITLE = 'Cropper';
-// }
 class TextManager {
   //for home screen
   static const String HOME_TITLE = 'Rabin-Karp Algorithm';
